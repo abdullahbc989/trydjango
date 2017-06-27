67bf04a1c834ba9d3d81847492603d2d443eb8a9
Abdullah Clute <clute.abdullahb@gmail.com>
2017-06-27 10:44:33 -0500

    Updated and Finished

--- Django_Project/Treasuregram/main_app/views.py
+++ Django_Project/Treasuregram/main_app/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Treasure
+"""If you want to import a specific class from your current app,
+you can leave off the package and type the following:
+from .module import class (or function)"""
 
 # Create your views here.
 def index(request):
-    return HttpResponse('<h1>Hello World!</h1>')
+    treasures = Treasure.objects.all()
+    return render(request, 'index.html', {'treasures':treasures})
+
+
+"""class Treasure:
+    def __init__(self, name, value, material, location, img_url):
+        self.name = name
+        self.value = value
+        self.material = material
+        self.location = location
+        self.img_url = img_url
+
+treasures = [
+    Treasure('Gold Nugget', 500.00, 'gold', "Curly's Creek, NM", 'static/imgs/gold.png'),
+    Treasure("Fool's Gold", 0, 'pyrite', "Fool's Falls, CO", 'static/imgs/fools_gold.jpg'),
+    Treasure('Coffee Can', 20.00, 'tin', 'Acme, CA', 'static/imgs/cof_cup.png'),
+]"""
